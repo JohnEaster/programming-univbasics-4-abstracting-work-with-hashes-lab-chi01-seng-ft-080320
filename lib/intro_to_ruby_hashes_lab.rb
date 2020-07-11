a6153949cc087d6fa470d4506599a09c52124c16
John Easter <john@owltestprep.com>
2020-07-11 14:37:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,6 @@ def read_from_hash(hash = { mother: "Kathryn", father: "Eric", brother: "Seth"},
   hash[key]
 end
 
-def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+def update_counting_hash(hash = { ants: 3, bees: 7, cats: 5, dogs: 2 }, key = :cats)
+  hash[:cats += 1]
 end
